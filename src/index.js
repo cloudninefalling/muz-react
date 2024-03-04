27b4a26c4f186aa2adcd7bd96fa4d0cbbe5a3def
cloudninefalling <shalygin.da7@gmail.com>
2024-03-04 09:06:32 +0300

    fix: basename for gh-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ScrollToTop from "./components/ScrollToTop";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}/muz-react`}>
       <ScrollToTop />
       <App />
     </BrowserRouter>
